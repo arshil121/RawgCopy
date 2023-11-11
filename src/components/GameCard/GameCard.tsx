3a5b3e8348b4ca12c836f339e53c4fc84f588548
shoab-alam <arshilalam121@gmail.com>
2023-11-11 18:19:58 +0600

    optimized images using crop

--- src/components/GameCard/GameCard.tsx
+++ src/components/GameCard/GameCard.tsx
@@ -1,7 +1,8 @@
-import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
+import { Card, CardBody, HStack, Heading, Image } from "@chakra-ui/react";
 import { Game } from "../../hooks/useGames";
 import PlatformIconList from "../platformIcon/PlatformIconList";
 import CriticScore from "../CriticScore/CriticScore";
+import getCroppedImageUrl from "../../services/image-url";
 
 interface Props {
   game: Game;
@@ -10,10 +11,10 @@ interface Props {
 const GameCard = ({ game }: Props) => {
   return (
     <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image} />
+      <Image src={getCroppedImageUrl(game.background_image)} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent='space-between'>
+        <HStack justifyContent="space-between">
           <PlatformIconList
             platforms={game.parent_platforms.map((p) => p.platform)}
           />
---
+++ src/services/image-url.ts
@@ -0,0 +1,19 @@
+const getCroppedImageUrl = (url: string): string => {
+  const target = "media/";
+  const index = url.indexOf(target);
+
+  if (index !== -1) {
+    const endIndex = index + target.length;
+    const baseUrl = url.slice(0, endIndex);
+    const restUrl = url.slice(endIndex);
+
+    // Append "/crop/600/400" to the base URL
+    return `${baseUrl}crop/600/400/${restUrl}`;
+  }
+
+  // Handle the case where "media/" is not found in the URL
+  console.error('Invalid URL format. Unable to locate "media/" segment.');
+  return url;
+};
+
+export default getCroppedImageUrl;
