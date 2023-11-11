6a3ec804dcecd8db838664ea1ec495a0a41d7466
shoab-alam <arshilalam121@gmail.com>
2023-11-11 20:10:34 +0600

    display genres and fix css issues

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,14 +1,30 @@
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
 import useGenres from "../../hooks/useGenre";
+import getCroppedImageUrl from "../../services/image-url";
 
 export const GenreList = () => {
   const { data } = useGenres();
+
   return (
     <>
-      <ul>
-        {data.map((genre) => (
-          <li key={genre.id}>{genre.name}</li>
+      <List>
+        {data?.map((genre) => (
+          // Ensure genre is defined before rendering
+          genre && (
+            <ListItem key={genre.id} paddingY='5px'>
+              <HStack>
+                <Image
+                  boxSize="32px"
+                  borderRadius={8}
+                  src={getCroppedImageUrl(genre.image_background)}
+                  alt={`${genre.name} Background`}
+                />
+                <Text fontSize='lg'>{genre.name}</Text>
+              </HStack>
+            </ListItem>
+          )
         ))}
-      </ul>
+      </List>
     </>
   );
 };
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,12 +1,11 @@
-
-
 import useData from "./useData/useData";
 
 export interface Genre {
   id: number;
   name: string;
+  image_background: string;
 }
 
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => useData<Genre>("/genres");
 
 export default useGenres;
